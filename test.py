d9e05c1c3455f1a572c105d471affe4a61844270
斯缪 <simiu_0609@163.com>
2016-06-15 08:20:19 +0800

    change filename

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 # gusimiu@baidu.com
 # 
 
-import network_tensorflow
+import nnet_tf
 from ml_reader import DataReader
 
 def test_Network():
@@ -15,8 +15,8 @@ def test_Network():
     Y = R.label
     Y.shape = (25, 1)
 
-    N = network_tensorflow.ConfigNetwork('conf/net.conf', 'embeddings')
-    for i in range(500):
+    N = nnet_tf.ConfigNetwork('conf/net.conf', 'embeddings')
+    for i in range(1000):
         cost = N.fit(A, B, Y)
         print cost
 
